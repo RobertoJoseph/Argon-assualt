339e9ac42cf348ec07365592c5227c7695c3f5fa
rubyz <robyamama55@gmail.com>
2020-04-10 22:42:10 +0200

    Adding SkyBox And Music Sound

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MusicPlayer : MonoBehaviour
+    
+
+{
+    AudioSource audioSource;
+    [SerializeField] float LoadLevelDelay = 1f;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+       
+    }
+
+    // Start is called before the first frame update
+
+    void Start()
+    {
+      
+        audioSource = GetComponent<AudioSource>();
+        Invoke("LoadNextLevel", LoadLevelDelay);
+        
+       
+
+        
+    }
+
+    // Update is called once per frame
+   
+    void Update()
+    {
+        
+        
+        
+    }
+    void LoadNextLevel()
+    {
+       
+        SceneManager.LoadScene(1);
+    }
+}
